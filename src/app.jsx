f3f2d67e42f656ad2482a6c771822ca47f9432eb
UBAID RAZA <ubaidraza1@gmail.com>
2025-07-23 12:33:51 +0500

    push

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import MunchysGrillWebsite from './components/MunchysGrillWebsite'
+
+function App() {
+  return (
+    <div className="App">
+      <MunchysGrillWebsite />
+    </div>
+  )
+}
+
+export default App
